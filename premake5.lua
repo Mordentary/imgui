da6e233df6a0e54d34412d34dd0af515f616319a
Mordentary <80980628+Mordentary@users.noreply.github.com>
2022-07-26 22:58:57 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,14 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+	cppdialect "C++17"
+	staticruntime "On"
+
+
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
 
 	files
 	{
@@ -13,22 +17,21 @@ project "ImGui"
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
-		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
+		"imgui_tables.cpp",
 		"imgui_demo.cpp"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
+	
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
 
 	filter "configurations:Debug"
 		runtime "Debug"
@@ -37,8 +40,3 @@ project "ImGui"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
